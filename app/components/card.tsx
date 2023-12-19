7ea2b30ac2d6ba12ead10fb9052d7c4b84e73ffb
Donato <donatotuzzolino@gmail.com>
2023-12-19 12:30:16 +0100

    fix

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -23,7 +23,7 @@ export default function Card({ product }: { product: Product }) {
         <div className="flex flex-col relative">
           <Image src={product?.images[0]} alt="item" width={289} height={220} />
           <div className=" text-center p-4 flex flex-col justify-center gap-[5px]">
-            <p className="text-[18px] font-[700]">{product?.title}</p>
+            <p className="text-[18px] font-[700] text-ellipsis whitespace-nowrap overflow-hidden">{product?.title}</p>
             <p className="text-[20px] font-[600] text-[#E32119]">{`${calculateDiscount(
               product?.price,
               discount,
